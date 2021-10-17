c4291595d55a52753d7d079f298ab943f93c902a
Eduardo <el_alienx@hotmail.com>
2021-10-17 13:12:47 +0200

    added the base firebase file

---
+++ src/scripts/firebase.js
@@ -0,0 +1,14 @@
+// NPM package
+import { initializeApp } from "firebase/app";
+
+// Using the Project fe-firebase-setup credentials
+const firebaseConfiguration = {
+  apiKey: "AIzaSyCg5AgfTsO8rtIQ5QyQ66RbuioeG0dcvuQ",
+  authDomain: "fe-firebase-setup.firebaseapp.com",
+  projectId: "fe-firebase-setup",
+  storageBucket: "fe-firebase-setup.appspot.com",
+  messagingSenderId: "466939589225",
+  appId: "1:466939589225:web:02ac6c3e8770e9a000a8f8",
+};
+
+const firebaseInstance = initializeApp(firebaseConfiguration);
