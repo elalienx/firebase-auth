7d283a04ad19a160abc60113629a98eaf307a7a7
Eduardo <el_alienx@hotmail.com>
2021-10-18 17:37:17 +0200

    fix bug

--- src/state/UserProvider.jsx
+++ src/state/UserProvider.jsx
@@ -6,7 +6,7 @@ const UserContext = createContext(null);
 
 export function UserProvider({ children }) {
   // Local state
-  const [user, setUser] = useState({ isLogged: false });
+  const [user, setUser] = useState({ isLogged: true });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
