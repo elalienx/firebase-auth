30a13760e589d41325d0d1d5c3ab20c44aed9cfd
Eduardo <el_alienx@hotmail.com>
2021-10-20 20:08:52 +0200

    remove bad code from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,18 @@
 // NPM packages
 import { useHistory } from "react-router-dom";
-import { signOut } from "firebase/auth";
 
 // Project files
 import { useAuth } from "state/AuthProvider";
-import { authInstance } from "scripts/firebase";
 
 export default function Home() {
   // Global state
-  const { user, setIsLogged } = useAuth();
+  const { user } = useAuth();
   const history = useHistory();
 
   // Methods
   function onLogout() {
     alert("We need to truly logout...");
     history.push("/login");
-
-    signOut(authInstance)
-      .then(() => {
-        alert("user logout");
-        setIsLogged(false);
-      })
-      .catch((error) => {
-        console.error("error", error);
-      });
   }
 
   return (
